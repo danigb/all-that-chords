93452f56a6340bbc72c63645c276ed2671a875d7
danigb <danigb@gmail.com>
2015-05-12 15:20:31 +0200

    Chord voicings

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # all-that-chords
+
 A library of music chords
+
+```js
+
+var chord = require('all-that-chords');
+
+var c = chord('Cmaj7');
+c.notes() // -> return an array of notes in pitch notation
+c.voicings() // -> ['left-hand-A', 'left-hand-B'...]
+c.voicing(c.voicings()[0]); // -> an array of notes
+```
--- test/chord.js
+++ test/chord.js
@@ -9,8 +9,17 @@ vows.describe('All that chords').addBatch({
     c = chord("Cmaj7");
     assert.equal(str(c.root), 'c4');
   },
-  "find notes": function() {
+  "notes": function() {
     c = chord("Cmaj7");
-    assert.equal(c.notes().map(str), ['c4', 'e4', 'g4', 'b4']);
+    assert.deepEqual(c.notes().map(str), ['c4', 'e4', 'g4', 'b4']);
+  },
+  "voicings": function() {
+    c = chord("Cmaj7");
+    assert.deepEqual(c.voicings(), ['left-hand-A', 'left-hand-B', 'left-hand-C', 'two-hand-A', 'two-hand-B', 'two-hand-C']);
+  },
+  "voicing": function() {
+    c = chord("Cmaj7");
+    assert.deepEqual(c.voicing('left-hand-A').map(str), ['e4', 'g4', 'b4']);
+    assert.deepEqual(c.voicing('left-hand-C').map(str), ['g4', 'b4', 'e5']);
   }
 }).export(module);
